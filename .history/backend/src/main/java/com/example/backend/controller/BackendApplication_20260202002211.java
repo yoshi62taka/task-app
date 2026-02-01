087a09d9882935313e3437e14adde8426da99e6b
yoshi <yoshitaka.yagawa@gmail.com>
2026-02-02 00:40:55 +0900

    環境構築②

---
+++ .history/backend/src/main/java/com/example/backend/controller/BackendApplication_20260202002211.java
@@ -0,0 +1,13 @@
+package com.example.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
